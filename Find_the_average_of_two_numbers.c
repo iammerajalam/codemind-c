b31279498b854c4d677d6156452d4e4c513ebdef
Meraj Alam <97613228+iammerajalam@users.noreply.github.com>
2022-02-11 22:16:39 +0545

    #include<stdio.h> #include<math.h> int main() {     float num1,num2;     float avg;     scanf("%f%f",&num1,&num2);     avg=(num1+num2)/2;     printf("%.4f",avg);     return 0; }

---
+++ Find_the_average_of_two_numbers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float num1,num2;
+    float avg;
+    scanf("%f%f",&num1,&num2);
+    avg=(num1+num2)/2;
+    printf("%.4f",avg);
+    return 0;
+}
